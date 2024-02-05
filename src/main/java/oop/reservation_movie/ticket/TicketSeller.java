f153d97ab64e4c9c173d5917cce778d77c1a02ca
meridaKim <sujin7187@naver.com>
2024-02-05 17:15:30 +0900

    [UPDATE] Object example.01 TicketOffice의 자율성 보장

--- src/main/java/oop/reservation_movie/ticket/TicketSeller.java
+++ src/main/java/oop/reservation_movie/ticket/TicketSeller.java
@@ -7,8 +7,8 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
     public void sellTo(Audience audience){
-        //매표소에 접근할 수 있는 판매원이 티켓을 가져다주고 구매에 대한 결정은 관객이 처리한다. 관객이 구매했을 경우의 금액을 매표소에 전달한다.
-        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
+        //이제 판매원은 관객과 접근만 하면 가방의 구매절차와 office의 현금수금절차에 직접 접근하지 않아도 티켓을 판매할 수 있다.
+        ticketOffice.sellTicketTo(audience);
     }
 
     public TicketOffice getTicketOffice() {
