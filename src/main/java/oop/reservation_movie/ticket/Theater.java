675cf5f760077038571205df0decdbff0371baa7
meridaKim <sujin7187@naver.com>
2024-02-05 17:00:31 +0900

    [UPDATE] Object example.01 TicketSeller의 자율성 보장

--- src/main/java/oop/reservation_movie/ticket/Theater.java
+++ src/main/java/oop/reservation_movie/ticket/Theater.java
@@ -8,16 +8,7 @@ public class Theater {
     }
 
     public void enter(Audience audience){
-        //invitation이 존재할 때 -> 요금을 내지 않는다.
-        if(audience.getBag().hasInvitation()){
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        //invitation이 없을 때 티켓을 구매하기 위해 요금을 낸다. 매표소에 요금을 저장
-        }else{
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        // ticketOffice에 더이상 접근하지 않는다. -> 자율성 보장
+        ticketSeller.sellTo(audience);
     }
 }
--- src/main/java/oop/reservation_movie/ticket/TicketSeller.java
+++ src/main/java/oop/reservation_movie/ticket/TicketSeller.java
@@ -6,6 +6,19 @@ public class TicketSeller {
     public TicketSeller(TicketOffice ticketOffice){
         this.ticketOffice = ticketOffice;
     }
+    public void sellTo(Audience audience){
+        //invitation이 존재할 때 -> 요금을 내지 않는다.
+        if(audience.getBag().hasInvitation()){
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+            //invitation이 없을 때 티켓을 구매하기 위해 요금을 낸다. 매표소에 요금을 저장
+        }else{
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+    }
 
     public TicketOffice getTicketOffice() {
         return ticketOffice;
