919c66020d35d022b9e4519983137e74fef1a9b5
meridaKim <sujin7187@naver.com>
2024-03-04 10:33:52 +0900

    [FIX] 오브젝트 11장 합성과 유연한 설계 실습 예제(부가 정책 합성하)

---
+++ src/main/java/oop/example/com/dry_principle/TaxablePolicy.java
@@ -0,0 +1,15 @@
+package oop.example.com.dry_principle;
+
+public class TaxablePolicy extends AdditionalRatePolicy {
+    private double taxRatio;
+
+    public TaxablePolicy(double taxRatio, RatePolicy next) {
+        super(next);
+        this.taxRatio = taxRatio;
+    }
+
+    @Override
+    protected Money afterCalculated(Money fee) {
+        return fee.plus(fee.times(taxRatio));
+    }
+}
--- src/main/java/oop/example/com/dry_principle/TaxableRegularPhone.java
+++ src/main/java/oop/example/com/dry_principle/TaxableRegularPhone.java
@@ -1,17 +1,17 @@
-package oop.example.com.dry_principle;
-
-import java.time.Duration;
-
-public class TaxableRegularPhone extends RegularPhone {
-    private double taxRate;
-
-    public TaxableRegularPhone(Money amount, Duration seconds, double taxRate) {
-        super(amount, seconds);
-        this.taxRate = taxRate;
-    }
-
-    @Override
-    protected Money afterCalculated(Money fee) {
-        return fee.plus(fee.times(taxRate));
-    }
-}
+//package oop.example.com.dry_principle;
+//
+//import java.time.Duration;
+//
+//public class TaxableRegularPhone extends RegularPhone {
+//    private double taxRate;
+//
+//    public TaxableRegularPhone(Money amount, Duration seconds, double taxRate) {
+//        super(amount, seconds);
+//        this.taxRate = taxRate;
+//    }
+//
+//    @Override
+//    protected Money afterCalculated(Money fee) {
+//        return fee.plus(fee.times(taxRate));
+//    }
+//}
